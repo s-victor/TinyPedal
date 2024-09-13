250d0a21424b3352d8f36036a6eb6c7c00e5a951
Xiang <flyinwhite@outlook.com>
2024-09-13 23:01:53 +0800

    add "primary preset" setter and "auto load preset" toggle

--- tinypedal/ui/preset_view.py
+++ tinypedal/ui/preset_view.py
@@ -32,8 +32,10 @@ from PySide2.QtWidgets import (
     QHBoxLayout,
     QLabel,
     QPushButton,
+    QCheckBox,
     QMenu,
     QListWidget,
+    QListWidgetItem,
     QMessageBox,
     QDialog,
     QLineEdit,
@@ -50,6 +52,11 @@ QSS_LISTBOX = (
     "QListView {font-size: 16px;outline: none;}"
     "QListView::item {height: 28px;border-radius: 0;}"
     "QListView::item:selected {selection-color: #FFF;background: #F20;}"
+)
+QSS_TAGGED_ITEM = "font-size: 14px;color: #FFF;"
+QSS_TAGGED_COLOR = (
+    "margin: 4px 4px 4px 0px;background: #F20;",  # LMU
+    "margin: 4px 4px 4px 0px;background: #0AF;",  # RF2
 )
 preset_name_valid = QRegularExpressionValidator(QRegularExpression('[^\\\\/:*?"<>|]*'))
 
@@ -73,6 +80,11 @@ class PresetList(QWidget):
         self.listbox_preset.setCurrentRow(0)
         self.listbox_preset.itemDoubleClicked.connect(self.load_preset)
 
+        # Check box
+        self.checkbox_autoload = QCheckBox("Auto load primary preset")
+        self.checkbox_autoload.setChecked(cfg.application["enable_auto_load_preset"])
+        self.checkbox_autoload.toggled.connect(self.toggle_autoload)
+
         # Button
         button_load = QPushButton("Load")
         button_load.clicked.connect(self.load_preset)
@@ -89,6 +101,7 @@ class PresetList(QWidget):
 
         layout_main.addWidget(self.label_loaded)
         layout_main.addWidget(self.listbox_preset)
+        layout_main.addWidget(self.checkbox_autoload)
         layout_button.addWidget(button_load)
         layout_button.addWidget(button_refresh)
         layout_button.addStretch(stretch=1)
@@ -101,11 +114,18 @@ class PresetList(QWidget):
 
     def refresh_list(self):
         """Refresh preset list"""
-        temp_list = cfg.load_preset_list()
-        if temp_list != self.preset_list:
-            self.preset_list = temp_list
-            self.listbox_preset.clear()
-            self.listbox_preset.addItems(self.preset_list)
+        self.preset_list = cfg.load_preset_list()
+        self.listbox_preset.clear()
+
+        for preset_name in self.preset_list:
+            # Add preset name
+            item = QListWidgetItem()
+            item.setText(preset_name)
+            self.listbox_preset.addItem(item)
+            # Add primary preset tag
+            label_item = PrimaryPresetTag(preset_name)
+            self.listbox_preset.setItemWidget(item, label_item)
+
         self.label_loaded.setText(
             f"Loaded: <b>{cfg.filename.last_setting[:-5]}</b>")
 
@@ -125,20 +145,39 @@ class PresetList(QWidget):
         _dialog = CreatePreset(self, title="Create new default preset")
         _dialog.open()
 
+    @staticmethod
+    def toggle_autoload(checked):
+        """Toggle auto load preset"""
+        cfg.application["enable_auto_load_preset"] = checked
+        cfg.save(filetype="config")
+
     def context_menu(self, position):
         """Preset context menu"""
         if self.listbox_preset.itemAt(position):
             menu = QMenu()
+            option_preset_lmu = menu.addAction("Set primary for LMU")
+            option_preset_rf2 = menu.addAction("Set primary for RF2")
             option_duplicate = menu.addAction("Duplicate")
             option_rename = menu.addAction("Rename")
             option_delete = menu.addAction("Delete")
 
             action = menu.exec_(self.listbox_preset.mapToGlobal(position))
             selected_index = self.listbox_preset.currentRow()
-            selected_filename = f"{self.preset_list[selected_index]}.json"
+            selected_preset_name = self.preset_list[selected_index]
+            selected_filename = f"{selected_preset_name}.json"
 
+            # Set primary preset LMU
+            if action == option_preset_lmu:
+                cfg.user.config["primary_preset"]["LMU"] = selected_preset_name
+                cfg.save(filetype="config")
+                self.refresh_list()
+            # Set primary preset RF2
+            elif action == option_preset_rf2:
+                cfg.user.config["primary_preset"]["RF2"] = selected_preset_name
+                cfg.save(filetype="config")
+                self.refresh_list()
             # Duplicate preset
-            if action == option_duplicate:
+            elif action == option_duplicate:
                 _dialog = CreatePreset(
                     self,
                     title="Duplicate Preset",
@@ -260,3 +299,24 @@ class CreatePreset(QDialog):
         # Close window
         self.accept()
         return None
+
+
+class PrimaryPresetTag(QWidget):
+    """Primary preset tag"""
+
+    def __init__(self, preset_name: str):
+        super().__init__()
+        layout_item = QHBoxLayout()
+        layout_item.setContentsMargins(0,0,0,0)
+        layout_item.setSpacing(0)
+        layout_item.addStretch(stretch=1)
+
+        primary_preset_dict = cfg.user.config["primary_preset"].items()
+        for sim_name, primary_preset in primary_preset_dict:
+            if preset_name == primary_preset:
+                label_sim_name = QLabel(sim_name)
+                label_sim_name.setStyleSheet(QSS_TAGGED_COLOR[sim_name == "RF2"])
+                layout_item.addWidget(label_sim_name)
+
+        self.setStyleSheet(QSS_TAGGED_ITEM)
+        self.setLayout(layout_item)
