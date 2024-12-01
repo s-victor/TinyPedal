30b91c73923092ba010d9ef4635c1dda4b7ae305
Xiang <flyinwhite@outlook.com>
2024-12-01 21:36:41 +0800

    refactor Brake bias Widget, add brake migration class

--- tinypedal/widget/brake_bias.py
+++ tinypedal/widget/brake_bias.py
@@ -20,9 +20,6 @@
 Brake bias Widget
 """
 
-from PySide2.QtCore import Qt
-from PySide2.QtWidgets import QGridLayout
-
 from ..api_control import api
 from ..module_info import minfo
 from ._base import Overlay
@@ -36,6 +33,8 @@ class Realtime(Overlay):
     def __init__(self, config):
         # Assign base setting
         Overlay.__init__(self, config, WIDGET_NAME)
+        layout = self.set_grid_layout(gap=self.wcfg["bar_gap"])
+        self.set_primary_layout(layout=layout)
 
         # Config font
         font_m = self.get_font_metrics(
@@ -43,7 +42,6 @@ class Realtime(Overlay):
 
         # Config variable
         bar_padx = self.set_padding(self.wcfg["font_size"], self.wcfg["bar_padding"])
-        bar_gap = self.wcfg["bar_gap"]
         self.decimals_bias = max(self.wcfg["decimal_places_brake_bias"], 0)
         self.decimals_migt = max(self.wcfg["decimal_places_brake_migration"], 1)
         self.prefix_bias = self.wcfg["prefix_brake_bias"]
@@ -57,13 +55,6 @@ class Realtime(Overlay):
             font_size=self.wcfg["font_size"],
             font_weight=self.wcfg["font_weight"])
         )
-
-        # Create layout
-        layout = QGridLayout()
-        layout.setContentsMargins(0,0,0,0)  # remove border
-        layout.setSpacing(bar_gap)
-        layout.setAlignment(Qt.AlignLeft | Qt.AlignTop)
-        self.setLayout(layout)
 
         # Brake bias
         text_bbias = self.format_brake_bias(50)
@@ -100,12 +91,7 @@ class Realtime(Overlay):
 
         # Last data
         self.checked = False
-        self.last_bbias = None
-        self.last_bmigt = None
-        self.bpres_max = 0
-        self.bpres_scale = 1
-        # -1 = auto detect, 0 = front, 1 = rear
-        self.ebrake_alloc = self.wcfg["electric_braking_allocation"]
+        self.brake_bmigt = BrakeMigration(self.wcfg["electric_braking_allocation"])
 
     def timerEvent(self, event):
         """Update when vehicle on track"""
@@ -116,62 +102,35 @@ class Realtime(Overlay):
                 self.checked = True
 
             # Brake bias
-            bbias = api.read.brake.bias_front() * 100
-            self.update_bbias(bbias, self.last_bbias)
-            self.last_bbias = bbias
+            bbias = api.read.brake.bias_front()
+            self.update_bbias(self.bar_bbias, bbias)
 
             # Brake migration
             if self.wcfg["show_brake_migration"]:
-                raw_brake = api.read.input.brake_raw()
-                bpres = api.read.brake.pressure()
-                bpres_sum = sum(bpres)
-
-                if self.bpres_max < bpres_sum:
-                    self.bpres_max = bpres_sum
-                    self.bpres_scale = 2 / bpres_sum
-
-                if raw_brake > max(bpres) > 0:
-                    max_front = max(bpres[:2])
-                    max_rear = max(bpres[2:])
-
-                    # Auto detect electric braking allocation
-                    if (self.wcfg["electric_braking_allocation"] == -1 and
-                        minfo.hybrid.motorState == 3 and max_rear > 0):
-                        if max_front / max_rear < 0.25:
-                            self.ebrake_alloc = 0  # front ebrake
-                        elif max_front / max_rear > 4:
-                            self.ebrake_alloc = 1  # rear ebrake
-
-                    if self.ebrake_alloc == 0:
-                        bias_rear = max_rear * self.bpres_scale
-                        bias_front_live = 100 - bias_rear / raw_brake * 100
-                    else:
-                        bias_front = max_front * self.bpres_scale
-                        bias_front_live = bias_front / raw_brake * 100
-
-                    bmigt = max(bias_front_live - bbias, +0)
-                else:
-                    bmigt = 0
-                self.update_bmigt(bmigt, self.last_bmigt)
-                self.last_bmigt = bmigt
+                bmigt = self.brake_bmigt.calc(
+                    brake_raw=api.read.input.brake_raw(),
+                    brake_bias=bbias,
+                    brake_pres=api.read.brake.pressure()
+                )
+                self.update_bmigt(self.bar_bmigt, bmigt)
 
         else:
             if self.checked:
                 self.checked = False
-                self.bpres_max = 0
-                self.bpres_scale = 1
-                self.ebrake_alloc = self.wcfg["electric_braking_allocation"]
+                self.brake_bmigt.reset()
 
     # GUI update methods
-    def update_bbias(self, curr, last):
+    def update_bbias(self, target, data):
         """Brake bias"""
-        if curr != last:
-            self.bar_bbias.setText(self.format_brake_bias(curr))
+        if target.last != data:
+            target.last = data
+            target.setText(self.format_brake_bias(data * 100))
 
-    def update_bmigt(self, curr, last):
+    def update_bmigt(self, target, data):
         """Brake migration"""
-        if curr != last:
-            self.bar_bmigt.setText(self.format_brake_migt(curr))
+        if target.last != data:
+            target.last = data
+            target.setText(self.format_brake_migt(data * 100))
 
     # Additional methods
     def format_brake_bias(self, value):
@@ -185,3 +144,60 @@ class Realtime(Overlay):
         """Format brake migration"""
         reading = f"{value:.{self.decimals_migt}f}"[:2 + self.decimals_migt]
         return f"{self.prefix_migt}{reading}{self.suffix_migt}"
+
+
+class BrakeMigration:
+    """Brake migration detection & calculation"""
+
+    def __init__(self, ebrake_alloc: int) -> None:
+        """
+        Args:
+            ebrake_alloc: electric braking allocation, -1 = auto detect, 0 = front, 1 = rear.
+        """
+        self._bpres_max = 0
+        self._bpres_scale = 1
+        self._ebrake_alloc = ebrake_alloc
+        self._auto_detect = bool(ebrake_alloc == -1)
+
+    def calc(self, brake_raw: float, brake_bias: float, brake_pres: list) -> float:
+        """Calculate brake migration
+
+        Args:
+            brake_raw: raw brake input.
+            brake_bias: front brake bias (fraction).
+            brake_pres: brake pressure (4 tyres).
+
+        Returns:
+            Brake migration (fraction).
+        """
+        bpres_sum = sum(brake_pres)
+
+        if self._bpres_max < bpres_sum:
+            self._bpres_max = bpres_sum
+            self._bpres_scale = 2 / bpres_sum
+
+        if brake_raw > max(brake_pres) > 0:
+            max_front = max(brake_pres[:2])
+            max_rear = max(brake_pres[2:])
+
+            if self._auto_detect and minfo.hybrid.motorState == 3 and max_rear > 0:
+                max_brake_ratio = max_front / max_rear
+                if max_brake_ratio < 0.25:
+                    self._ebrake_alloc = 0  # front ebrake
+                elif max_brake_ratio > 4:
+                    self._ebrake_alloc = 1  # rear ebrake
+
+            if self._ebrake_alloc == 0:
+                bias_rear = max_rear * self._bpres_scale
+                bias_front_live = 100 - bias_rear / brake_raw
+            else:
+                bias_front = max_front * self._bpres_scale
+                bias_front_live = bias_front / brake_raw
+
+            return max(bias_front_live - brake_bias, +0)
+        return 0
+
+    def reset(self):
+        """Reset"""
+        self._bpres_max = 0
+        self._bpres_scale = 1
