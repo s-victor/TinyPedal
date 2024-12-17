cff315aa322fe5ba57a6d65b25e2a04ad360a169
Xiang <flyinwhite@outlook.com>
2024-12-18 03:22:44 +0800

    refactor RestAPI module

--- tinypedal/module/module_restapi.py
+++ tinypedal/module/module_restapi.py
@@ -27,6 +27,8 @@ import json
 import re
 import socket
 from urllib.request import urlopen
+from typing import Any
+from collections.abc import Callable
 
 from ._base import DataModule
 from ..module_info import minfo
@@ -80,12 +82,13 @@ class Realtime(DataModule):
 
     def __init__(self, config):
         super().__init__(config, MODULE_NAME)
-        self.task_delete = set()
+        self.task_deletion = set()
 
     def update_data(self):
         """Update module data"""
         reset = False
         update_interval = self.active_interval
+
         sorted_task_runonce = {}
         sorted_task_repeats = {}
 
@@ -95,50 +98,46 @@ class Realtime(DataModule):
                 if not reset:
                     reset = True
                     update_interval = self.active_interval
-                    self.task_delete.clear()
-
-                    (sim_name, url_rest, time_out, retry, retry_delay
-                     ) = self.__connection_setup()
-                    sorted_task_runonce = sort_tasks(sim_name, TASK_RUNONCE)
-                    sorted_task_repeats = sort_tasks(sim_name, TASK_REPEATS)
-                    # Run all tasks once per garage out, and check availability
-                    if sorted_task_runonce:
-                        asyncio.run(self.__task_runonce(
-                            sorted_task_runonce, url_rest, time_out, retry, retry_delay))
-                        self.__remove_unavailable_task(sorted_task_runonce)
-                    if sorted_task_repeats:
-                        asyncio.run(self.__task_runonce(
-                            sorted_task_repeats, url_rest, time_out, retry, retry_delay))
-                        self.__remove_unavailable_task(sorted_task_repeats)
-
-                # Run repeatedly while on track
-                if sorted_task_repeats:
-                    asyncio.run(self.__task_repeats(sorted_task_repeats, url_rest, time_out))
+                    sim_name = api.read.check.sim_name()
+                    sort_tasks(sim_name, TASK_RUNONCE, sorted_task_runonce)
+                    sort_tasks(sim_name, TASK_REPEATS, sorted_task_repeats)
+                    self.__run_tasks(sim_name, sorted_task_runonce, sorted_task_repeats)
 
             else:
                 if reset:
                     reset = False
                     update_interval = self.idle_interval
-                    reset_to_default((sorted_task_runonce, sorted_task_repeats))
+                    # Reset when finished
+                    reset_to_default(sorted_task_runonce)
+                    reset_to_default(sorted_task_repeats)
 
         # Reset to default on close
-        reset_to_default((sorted_task_runonce, sorted_task_repeats))
+        reset_to_default(sorted_task_runonce)
+        reset_to_default(sorted_task_repeats)
 
-    def __remove_unavailable_task(self, active_task: dict) -> None:
-        """Remove unavailable task"""
-        for resource_name in self.task_delete:
-            if resource_name in active_task:
-                active_task.pop(resource_name, None)
-                logger.info("Rest API: %s unavailable", resource_name.upper())
+    def __run_tasks(self, sim_name: str, task_runonce: dict, task_repeats: dict):
+        """Run tasks"""
+        self.task_deletion.clear()
+        url_rest, time_out, retry, retry_delay = self.__setup_connection(sim_name)
+        # Run all tasks once per garage out, and check availability
+        if task_runonce:
+            asyncio.run(self.__task_runonce(
+                task_runonce, url_rest, time_out, retry, retry_delay))
+            remove_unavailable_task(task_runonce, self.task_deletion)
+        if task_repeats:
+            asyncio.run(self.__task_runonce(
+                task_repeats, url_rest, time_out, retry, retry_delay))
+            remove_unavailable_task(task_repeats, self.task_deletion)
+        # Run repeatedly while on track
+        if task_repeats:
+            asyncio.run(self.__task_repeats(task_repeats, url_rest, time_out))
 
-    def __connection_setup(self) -> tuple:
+    def __setup_connection(self, sim_name: str) -> tuple[str, float, int, float]:
         """Connection setup"""
         url_host = self.mcfg["url_host"]
         time_out = min(max(self.mcfg["connection_timeout"], 0.5), 10)
-        retry = min(max(self.mcfg["connection_retry"], 0), 10)
+        retry = min(max(int(self.mcfg["connection_retry"]), 0), 10)
         retry_delay = min(max(self.mcfg["connection_retry_delay"], 0), 60)
-        sim_name = api.read.check.sim_name()
-
         if sim_name == "LMU":
             url_port = self.mcfg["url_port_lmu"]
             url_rest = f"http://{url_host}:{url_port}/rest/"
@@ -146,49 +145,59 @@ class Realtime(DataModule):
             url_port = self.mcfg["url_port_rf2"]
             url_rest = f"http://{url_host}:{url_port}/rest/"
         else:
-            logger.info("Rest API: game session not found")
+            logger.info("RestAPI: game session not found")
             url_rest = ""
-        return sim_name, url_rest, time_out, retry, retry_delay
+        return url_rest, time_out, retry, retry_delay
 
-    async def __task_runonce(self, active_task: dict, url: str,
-        time_out: int, retry: int, retry_delay: float) -> any:
+    async def __task_runonce(self, active_task: dict, url_rest: str,
+        time_out: int, retry: int, retry_delay: float):
         """Update task runonce"""
-        if not url:
+        if not url_rest:
             return None
-        tasks = (self.__fetch_retry(url, time_out, retry, retry_delay, resource_name, active_task[resource_name])
-                 for resource_name in active_task)
+        tasks = (
+            self.__fetch_retry(
+                url_rest, time_out, retry, retry_delay, resource_name, output_set
+            )
+            for resource_name, output_set in active_task.items()
+        )
         return await asyncio.gather(*tasks)
 
-    async def __task_repeats(self, active_task: dict, url: str, time_out: int) -> any:
+    async def __task_repeats(self, active_task: dict, url_rest: str, time_out: int):
         """Update task repeatedly"""
-        if not url:
+        if not url_rest:
             return None
-        tasks = (self.__fetch(url, time_out, resource_name, active_task[resource_name])
-                 for resource_name in active_task)
+        tasks = (
+            self.__fetch(url_rest, time_out, resource_name, output_set)
+            for resource_name, output_set in active_task.items()
+        )
         return await asyncio.gather(*tasks)
 
     async def __fetch(self, url_rest: str, time_out: int,
-        resource_name: str, output_set: tuple) -> None:
+        resource_name: str, output_set: tuple):
         """Fetch data without retry"""
-        resource_output = get_resource(f"{url_rest}{resource_name}", time_out)
-        if isinstance(resource_output, dict):
-            for output in output_set:
-                get_value(resource_output, *output)
+        full_url = f"{url_rest}{resource_name}"
+        while not self._event.is_set() and self.state.active:
+            resource_output = get_resource(full_url, time_out)
+            if isinstance(resource_output, dict):
+                for output in output_set:
+                    get_value(resource_output, *output)
+            await asyncio.sleep(self.active_interval)
 
     async def __fetch_retry(self, url_rest: str, time_out: int, retry: int,
-        retry_delay: float, resource_name: str, output_set: tuple) -> None:
+        retry_delay: float, resource_name: str, output_set: tuple):
         """Fetch data with retry"""
         data_available = False
+        total_retry = retry
         full_url = f"{url_rest}{resource_name}"
-        while not self._event.wait(0) and retry >= 0:
+        while not self._event.is_set() and retry >= 0:
             resource_output = get_resource(full_url, time_out)
             # Verify & retry
             if not isinstance(resource_output, dict):
-                logger.info("Rest API: %s %s, %s retry",
-                    resource_name.upper(), resource_output, retry)
+                logger.info("RestAPI: ERROR: %s %s (%s/%s retries left)",
+                    resource_name.upper(), resource_output, retry, total_retry)
                 retry -= 1
                 if retry < 0:  # add to unavailable task delete list
-                    self.task_delete.add(resource_name)
+                    self.task_deletion.add(resource_name)
                     break
                 await asyncio.sleep(retry_delay)
                 continue
@@ -198,25 +207,33 @@ class Realtime(DataModule):
                     data_available = True
             # Add to unavailable task delete list
             if not data_available:
-                self.task_delete.add(resource_name)
-            logger.info("Rest API: %s data updated", resource_name.upper())
+                self.task_deletion.add(resource_name)
+            logger.info("RestAPI: UPDATE: %s", resource_name.upper())
             break
 
 
-def reset_to_default(task_dict_list: tuple):
+def reset_to_default(active_task: dict):
     """Reset active task data to default"""
-    for active_task in task_dict_list:
-        for output_set in active_task.values():
+    if active_task:
+        for resource_name, output_set in active_task.items():
             for output in output_set:
                 setattr(output[0], output[1], output[2])
+            logger.info("RestAPI: RESET: %s", resource_name.upper())
+        active_task.clear()
+
+
+def remove_unavailable_task(active_task: dict, task_deletion: set):
+    """Remove unavailable task from deletion list"""
+    for resource_name in task_deletion:
+        if resource_name in active_task:
+            active_task.pop(resource_name, None)
+            logger.info("RestAPI: MISSING: %s", resource_name.upper())
 
 
 def get_resource(url: str, time_out: int) -> (dict | str):
     """Get resource from REST API"""
     try:
         with urlopen(url, timeout=time_out) as raw_resource:
-            if raw_resource.getcode() != 200:
-                raise ValueError
             return json.loads(raw_resource.read().decode("utf-8"))
     except (TypeError, AttributeError, KeyError, ValueError):
         return "data not found"
@@ -225,8 +242,8 @@ def get_resource(url: str, time_out: int) -> (dict | str):
 
 
 def get_value(
-    data: dict, target: object, output: str, default: any,
-    mod_func: object | None, *keys: tuple[str]) -> bool:
+    data: dict, target: object, output: str, default: Any,
+    mod_func: Callable | None, *keys: tuple[str, ...]) -> bool:
     """Get value from resource dictionary, fallback to default value if invalid"""
     for key in keys:
         data = data.get(key, None)
@@ -243,6 +260,8 @@ def get_value(
     return True
 
 
-def sort_tasks(sim_name: str, task_set: tuple) -> dict:
+def sort_tasks(sim_name: str, task_set: tuple, active_task: dict):
     """Sort task set into dictionary, key - resource_name, value - output_set"""
-    return {task[1]:task[2] for task in task_set if re.search(task[0], sim_name)}
+    for pattern, resource_name, output_set in task_set:
+        if re.search(pattern, sim_name):
+            active_task[resource_name] = output_set
